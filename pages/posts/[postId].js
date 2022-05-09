1bb2b99b3ed1decadb0652445aa7e833214907fe
Abhay Kishan Sharma <abhay@unthinkable.co>
2022-05-09 20:58:36 +0530

    Performing prerendering static generation with dynamic parameter

---
+++ pages/posts/[postId].js
@@ -0,0 +1,59 @@
+import React from "react";
+
+function Post({ post }) {
+  return (
+    <div>
+      <h2>
+        {post.id} {post.title}
+      </h2>
+      <p>{post.body}</p>
+    </div>
+  );
+}
+
+export default Post;
+
+// For fetching the params
+export async function getStaticPaths() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/posts");
+  const data = await response.json();
+
+  const paths = data.map((post) => {
+    return {
+      params: {
+        postId: `${post.id}`,
+      },
+    };
+  });
+
+  return {
+    // paths: [
+    //   {
+    //     params: { postId: "1" },
+    //   },
+    //   {
+    //     params: { postId: "2" },
+    //   },
+    //   {
+    //     params: { postId: "3" },
+    //   },
+    // ],
+    paths,
+    fallback: false,
+  };
+}
+
+// here context argument contains the params which fetch the url
+export async function getStaticProps(context) {
+  const { params } = context;
+  const response = await fetch(
+    `https://jsonplaceholder.typicode.com/posts/${params.postId}`
+  );
+  const data = await response.json();
+
+  return {
+    props: {
+      post: data,
+    },
+  };
+}
---
+++ pages/users.js
@@ -0,0 +1,31 @@
+import React from "react";
+import User from "../components/user";
+
+function UserList({ users }) {
+  return (
+    <div>
+      <h1>List of Users</h1>
+      {users.map((user) => {
+        return (
+          <div key={user.id}>
+            <User user={user} />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+export default UserList;
+
+// This function runs on server side not client side
+export async function getStaticProps() {
+  const response = await fetch("https://jsonplaceholder.typicode.com/users");
+  const data = await response.json();
+
+  return {
+    props: {
+      users: data,
+    },
+  };
+}
